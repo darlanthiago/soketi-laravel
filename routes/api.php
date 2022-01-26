b8388843eeebb3f810a8f9f584eb141536965c14
Darlan Thiago <darlanthiago53@gmail.com>
2022-01-26 13:13:25 -0300

    first commit

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Events\TestEvent;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/test-event', function () {
+
+    return event(new TestEvent('hello world'));
+
+});
+
